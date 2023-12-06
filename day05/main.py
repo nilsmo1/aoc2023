7bafa606cb1b6a2797a8567e496debc5060f7ccc
Nils Mo <nilsmo1@hotmail.com>
2023-12-06 01:32:51 +0100

    day05 created

---
+++ day05/main.py
@@ -0,0 +1,30 @@
+# Day 5, If You Give A Seed A Fertilizer
+
+# Q1
+
+# Q2
+
+# Input
+def parse_input(file):
+    with open(file, 'r') as inp:
+        return []
+
+
+if __name__ == '__main__':
+    # Samples
+    sample_input = parse_input('sample')
+
+    # Tests
+    t1 = 0
+    t2 = 0
+    assert t1 == 0, t1
+    assert t2 == 0, t2
+
+    # Puzzle input
+    puzzle_input = parse_input('puzzle-input')
+
+    # Results
+    q1 = 0
+    q2 = 0
+    print(f'Q1: {q1}')
+    print(f'Q2: {q2}')
